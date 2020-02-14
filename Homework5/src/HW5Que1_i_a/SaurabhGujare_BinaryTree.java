643dd667c4ad9dff6b17bf6cd717a8c17783968b
Saurabh Gujare <saurabhgujare@Saurabhs-MacBook-Pro.local>
2020-02-14 18:12:59 -0500

    Added Binary Tree, BST, 23tree, 234tree

---
+++ Homework5/src/HW5Que1_i_a/SaurabhGujare_BinaryTree.java
@@ -0,0 +1,4 @@
+package HW5Que1_i_a;
+
+public class SaurabhGujare_BinaryTree {
+}
---
+++ Homework5/src/HW5Que1_j/SaurabhGujare_BinaryTree.java
@@ -0,0 +1,8 @@
+package HW5Que1_j;
+
+/**
+ * @author Saurabh Gujare
+ * @NUID 001424874
+ */
+public class SaurabhGujare_BinaryTree {
+}
