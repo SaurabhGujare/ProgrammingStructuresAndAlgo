af1bfcb1bc8e0b5a8f65c55a6b284331f2819e49
SaurabhGujare <gujare.s@husky.neu.edu>
2020-06-27 00:48:37 -0400

    tmc Battleship game

--- LeetCode/src/BattleshipGame/Solution.java
+++ LeetCode/src/BattleshipGame/Solution.java
@@ -1,8 +1,51 @@
 package BattleshipGame;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Saurabh Gujare
  * @NUID 001424874
  */
 public class Solution {
+
+    public static String solution(int N, String S, String T) {
+
+        Set<String> hits = new HashSet<>(Arrays.asList(T.split(" ")));
+        String[] ships = T.split(",");
+        int sunk=0, hit=0;
+
+        for(String ship : ships){
+            Set<String> shipComponent = new HashSet<>();
+            char top = ship.charAt(0);
+            char left = ship.charAt(1);
+            char bottom = ship.charAt(3);
+            char right = ship.charAt(4);
+
+            for(char i=top;i<=bottom;i++){
+                for(char j=left;j<=right;j++){
+                    shipComponent.add(""+i+j);
+                }
+            }
+
+            if(hits.containsAll(shipComponent))
+                sunk+=1;
+            else{
+                for(String shipPart: shipComponent){
+                    if(hits.contains(shipPart))
+                        hit+=1;
+                }
+            }
+
+        }
+        return ""+sunk+","+hit;
+
+    }
+
+    public static void main(String[] args) {
+        //  String ans = solution(4,"1B 2C,2D 4D","2B 2D 3D 4D 4A");
+        String ans = solution(12, "1A 2A,12A 12A", "12A");
+        System.out.println(ans);
+    }
 }
